eadbe3a9f9e201612802175557b45f0fe7e98ccb
neucrack <CZD666666@gmail.com>
2016-12-11 01:48:22 +0800

    move script to js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,26 @@
 
 window.onload = function () {
-  document.getElementById("mail").onclick = function MailInfo(){
-    alert("\n\n\n\n\n\n\n\n\t\tCZD666666@gmail.com \n\n\n\n\n\n");
-  }
+	AddBackground();
+  	document.getElementById("mail").onclick = function MailInfo(){
+    	alert("\n\n\n\n\n\n\n\n\t\tCZD666666@gmail.com \n\n\n\n\n\n");
+  	}
 
 }
+
+function AddBackground(){//http://www.htmleaf.com/jQuery/Layout-Interface/201506182060.html
+    var number = 50;
+    var width = document.body.clientWidth;
+    if(width>1366)
+        number = 70;
+    else{
+      if(width > 500)
+        width = 500;
+      number = width*width*width/2000000;
+    }
+    $(".page-header-bg").jParticle({
+      background: " linear-gradient(120deg, #155799, #159957)",
+      color: "#E6E6E6",
+      particlesNumber: number
+    });
+};
 
